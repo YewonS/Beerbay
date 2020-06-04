18978c576e8c4a885d4c7afa1a5259f5a8cf4e24
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2020-06-04 14:47:11 +0200

    initialize front-end

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static("public"));
+app.use(express.static("views"));
+
+app.set('view engine', 'ejs');
+const ejs = require('ejs');
+
+
+/* Session */
+
+const session = require('express-session');
+app.use(session({
+    secret: require('./config/configSession.json').sessionSecret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        sameSite: true,
+        secure: false
+    }
+}));
+
+const rateLimit = require("express-rate-limit");
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 50 // limit each IP to 100 requests per windowMs
+});
+app.use("/login", limiter);
+app.use("/signup", limiter);
 
 
 /* Setup Objection + Knex */
@@ -38,6 +64,15 @@ app.use(ratingRoute);
 app.use(barRoute);
 app.use(collectionRoute);
 
+//TODO: How can I make complicated queries with joined tables?
+
+
+app.get('/', (req, res) => {
+    console.log("session: ", req.sessionID);
+    console.log("user: ", req.session.user);
+
+    return res.render('main');
+})
 
 
 /* Start server */
