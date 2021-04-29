bce3611d39ef6d7e29afbef261c391a4d688be33
Yewon Seo <yewonblairseo@gmail.com>
2021-04-29 20:03:48 +0200

    attempt to change routes

---
+++ routes/shops.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const Shop = require('../models/Shop.js');
+
+router.get('/api/shops', async (req, res) => {
+    const shop = await Shop.query().select();
+    return res.send({ response: shop });
+});
+
+router.get('/api/shops/name/:name', async (req, res) => {
+    const name = req.params.name;
+    const shopsFound = await Shop.query().select().where({ 'name': name }).limit(1);
+    if (shopsFound.length > 0) {
+        return res.send({ response: shopsFound });
+    } else {
+        return res.status(400).send({ response: "No bar of the name found." });
+    }
+
+});
+
+
+module.exports = router;
